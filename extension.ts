4256adf92bdae7c86aadcb5e07ec70a8509f1ec1
Emilie Bang Holmberg <emilie.b.holmberg@hotmail.dk>
2026-03-16 20:22:43 +0100

    Removed unused stuff from the main extension file

--- extension.ts
+++ extension.ts
@@ -1,16 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// import * as langData from "./snippets/language-data.json";
 import { getChannel, initLogger, log, show } from './client/src/logger';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, Trace } from 'vscode-languageclient/node';
 import path from 'path';
-import "./client/src/commands";
 import { runSimpleCommand, runWithInput } from './client/src/commands';
 
 let client: LanguageClient; // Language client instance for communicating with the language server
-
-// const snippets = langData.snippets; // File with snippets for the completion provider 
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -45,21 +41,6 @@ export function activate(context: vscode.ExtensionContext): void {
   ];
 
   context.subscriptions.push(...commands);
-
-
-
-
-  // -------------------- Initial stuff - Will problably be changed ---------------------
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('dynsrv-vscode.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from dynsrv-vscode!');
-  });
-  context.subscriptions.push(disposable);
 
 }
 
