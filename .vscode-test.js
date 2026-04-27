2f62193c2a94843363b268d889a1c52a3ef1f889
Emilie Bang Holmberg <emilie.b.holmberg@hotmail.dk>
2026-04-27 10:07:55 +0200

    Got the testing env working for the ts and integration test

--- .vscode-test.js
+++ .vscode-test.js
@@ -5,10 +5,10 @@ module.exports = defineConfig([
     label: "integrationTests",
     files: "dist/**/*.integration.test.js",
     version: "stable",
+    launchArgs: ["--disable-gpu"],
     mocha: {
       ui: "tdd",
       timeout: 20000,
     },
   },
 ]);
-
