df5044dce1001f944834e0f30277600f705f4e5b
Thomas Wright <tom.tdw@gmail.com>
2026-06-21 13:08:53 +0200

    Use external dsrv-lsp after repository split

--- extension.ts
+++ extension.ts
@@ -1,33 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { getChannel, initLogger, log, show } from './client/src/logger';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, Trace } from 'vscode-languageclient/node';
-import path from 'path';
 import { runSimpleCommand, runWithInput, runWithInputAndTypes, runWithTypes } from './client/src/commands';
-import * as fs from 'fs';
 
-let client: LanguageClient; // Language client instance for communicating with the language server
+let client: LanguageClient;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
   initLogger('DSRV');
-  log("DSRV extension activated");
+  log('DSRV extension activated');
   show();
 
   const outputChannel = getChannel();
 
-
   const serverOptions: ServerOptions = {
-    command: resolveServerExe(context),
+    command: resolveServerExe(),
     args: [],
     transport: TransportKind.stdio,
   };
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: [{ language: 'dsrv' }],
-    outputChannel: outputChannel,
+    outputChannel,
   };
 
   client = new LanguageClient('dsrv-lsp', 'DSRV LSP', serverOptions, clientOptions);
@@ -43,25 +36,11 @@ export function activate(context: vscode.ExtensionContext): void {
   ];
 
   context.subscriptions.push(...commands);
-
 }
 
 export function deactivate() { }
 
-
-// Helper function to get the path to the binary, checking both the config and the default locations.
-function resolveServerExe(context: vscode.ExtensionContext): string {
-  const debugPath = context.asAbsolutePath(path.join('target', 'debug', 'dsrv-lsp'));
-  const releasePath = context.asAbsolutePath(path.join('target', 'release', 'dsrv-lsp'));
-
-  if (fs.existsSync(debugPath)) {
-    return debugPath;
-  }
-
-  if (fs.existsSync(releasePath)) {
-    return releasePath;
-  }
-
-  throw new Error(`Could not find dsrv-lsp in ${debugPath} or ${releasePath}`);
-
+function resolveServerExe(): string {
+  const configuredPath = vscode.workspace.getConfiguration('DSRV').get<string>('lspPath')?.trim();
+  return configuredPath || 'dsrv-lsp';
 }
